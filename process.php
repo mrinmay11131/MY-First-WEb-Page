25157555ef05e0f7d90cf97c72fd18e288533640
mrinmay11131 <mrinmay11131@users.noreply.github.com>
2020-01-27 18:39:04 -0500

    add

---
+++ process.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['submit'])) {
+
+$to = "somebody@example.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$subject,$txt,$headers);
+}
+
+?>
